18237eeeabf47b189dcc40028182ee9ef635d73d
Daniel <truenorthtrainingcompany@gmail.com>
2016-06-28 14:07:46 -0400

    Return errors invalid Post Post must have text, returns error if invalid Cleaned up templates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,12 +9,17 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(params.require(:post).permit(:text, :text))
-		@post.save
-		redirect_to @post
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	private
