8394bb425fd4d5a8a53a41dbf3408ded00eb19f2
Daniel <truenorthtrainingcompany@gmail.com>
2016-06-29 12:17:11 -0400

    User id added to comment Migrated tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160629160030) do
+ActiveRecord::Schema.define(version: 20160629161544) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160629160030) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
