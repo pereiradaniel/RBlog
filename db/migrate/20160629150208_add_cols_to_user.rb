c2ea0d6d55ceb69b55789aee00e5791a78697c81
Daniel <truenorthtrainingcompany@gmail.com>
2016-06-29 11:04:33 -0400

    Add user model Add columns to user model for bcrypt Run migrations and add user model with columns

---
+++ db/migrate/20160629150208_add_cols_to_user.rb
@@ -0,0 +1,7 @@
+class AddColsToUser < ActiveRecord::Migration
+  def change
+  	add_column :users, :name, :string
+  	add_column :users, :email, :string
+  	add_column :users, :password_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628192734) do
+ActiveRecord::Schema.define(version: 20160629150208) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20160628192734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "text"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
