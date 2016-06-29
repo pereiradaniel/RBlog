8ee9b33b5bc44fc1e1b6dec66907c43d98e79684
Daniel <truenorthtrainingcompany@gmail.com>
2016-06-29 11:23:16 -0400

    Update routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get'/logout' => 'sessions#destroy'
   
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
